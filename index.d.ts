28debacba7fee66c2ca447fd7cdcaf16bd2927b3
ArathyKumar <ask6295@gmail.com>
2024-12-11 15:00:47 +0530

    update input config

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+declare module 'rhds-theme' {
+    // Define the types based on your module's functionality
+    export default function setVersion(version: number);
+    //export const someConstant: string;
+  
+    // For example, if your module exports an object:
+    // export default {
+    //   version: (arg: string) => void,
+    //   //someConstant: 'value'
+    // };
+  }
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
-module.exports = function version(version){
-    const root = document.documentElement;
+// module.exports = function setVersion(version){
+//     const root = document.documentElement;
 
+//     root.style.setProperty('--version', 10);
+// }
+
+exports.setVersion =  function (arg) {
+    const root = document.documentElement;
+    console.log("------- the argument value is", arg);
     root.style.setProperty('--version', 10);
 }
